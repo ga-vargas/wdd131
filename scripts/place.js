cb926c778fdcd4587d8ffb1db76595e78839cd44
ga-vargas <gavargas2001@gmail.com>
2024-10-10 15:25:08 -0300

    new changes

---
+++ scripts/place.js
@@ -0,0 +1,27 @@
+
+const year = new Date().getFullYear();
+const lastModified = document.lastModified;
+
+document.getElementById('lastModified').textContent = `Last Modification: ${lastModified}`;
+
+
+function calculateWindChill(temperature, windSpeed) {
+    if (temperature <= 10 && windSpeed > 4.8) {
+        const windChill = (
+            13.12 +
+            0.6215 * temperature -
+            11.37 * Math.pow(windSpeed, 0.16) +
+            0.3965 * temperature * Math.pow(windSpeed, 0.16)
+        );
+        return `${windChill.toFixed(1)}°C`;
+    } else {
+        return "N/A";
+    }
+}
+
+const temperature = 10;
+const windSpeed = 5;
+
+const windChill = calculateWindChill(temperature, windSpeed);
+
+document.getElementById('windChill').textContent = windChill;
