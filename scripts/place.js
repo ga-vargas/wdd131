2ba475ee0320b547277ae6c38decd1dbdcc54e22
ga-vargas <gavargas2001@gmail.com>
2024-10-10 15:37:12 -0300

    last change

--- scripts/place.js
+++ scripts/place.js
@@ -1,9 +1,7 @@
-
 const year = new Date().getFullYear();
 const lastModified = document.lastModified;
 
 document.getElementById('lastModified').textContent = `Last Modification: ${lastModified}`;
-
 
 function calculateWindChill(temperature, windSpeed) {
     if (temperature <= 10 && windSpeed > 4.8) {
